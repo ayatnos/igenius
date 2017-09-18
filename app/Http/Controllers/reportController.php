1dd6c7171680fb104131d5956bd974382302d0db
ayatnos <ayat_nos@hotmail.com>
2017-09-18 17:12:26 +0700

    Update reportController.php
    reportStudent

--- app/Http/Controllers/reportController.php
+++ app/Http/Controllers/reportController.php
@@ -16,15 +16,31 @@ class reportController extends Controller
   {
     $stdinfo = DB::table('student')->get();
 
-    return view('reportForm',['stdinfos' => $stdinfo]);
+    return view('reportForm');
   }
 
   function searchInfo(Request $req)
-  {
-    $result = $req->input('search');
+  { 
+
+    foreach ($_POST as $key => $value) {
+
+    }
+
+   if(!empty($key == 'search')){
+    $result = $req->input('insNickname');
 
     $data = DB::table('student')->where('std_nickname','LIKE','%'.$result.'%')->get();
 
     return view('reportForm',['datas'=>$data]);
+
+  }else if(!empty($key == 'select')) {
+
+    $stdSelect = $req->input('select');
+
+    $student = DB::table('student')->where('std_id','=',$stdSelect)->get();
+
+    return view('reportStudent',['students' =>$student]);
+
+  }
   }
 }
