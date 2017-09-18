ef68ac9c06c3f907a3ed34e7d2c27a6f672b9d64
ayatnos <ayat_nos@hotmail.com>
2017-09-18 17:15:53 +0700

    Update reportForm.blade.php
    report student

--- resources/views/reportForm.blade.php
+++ resources/views/reportForm.blade.php
@@ -13,15 +13,17 @@ tr td{padding: 10px;}
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
           <div style="background-color:#ffffff;padding:10px;border-radius:10px;">
+
           <form action="reportForm" method="post" >
             <input type="text" name="insNickname" size="50">
 
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <input type="submit" name="submit" value="ค้นหา" class="btn btn-primary">
+            <input type="submit" name="search" value="ค้นหา" class="btn btn-primary">
           </form>
         </div>
         <div style="background-color:#ffffff;padding:10px;border-radius:10px;margin-top:5px;">
-            <table>
+          <form action="reportForm" method="post" name="stdSelect">
+            <table >
               @if(!empty($datas))
               <tr>
                 <td>ลำดับ</td>
@@ -31,8 +33,10 @@ tr td{padding: 10px;}
                 <td>อายุ</td>
                 <td>ผู้ปกครอง</td>
                 <td>เบอร์โทรศัพท์</td>
+                <td>&nbsp;</td>
               </tr>
               @foreach($datas as $row)
+
               <tr>
                 <td>{{$row->std_id}}</td>
                 <td>{{$row->std_nickname}}</td>
@@ -41,7 +45,16 @@ tr td{padding: 10px;}
                 <td>{{$row->std_age}}</td>
                 <td>{{$row->std_parent_name}}</td>
                 <td>{{$row->std_parent_tel_1}}</td>
+                <td>
+
+                <label for="select{{$row->std_id}}" style="cursor:pointer;">เพิ่มเติม</label>
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input type="checkbox" name="select" value="{{$row->std_id}}"
+                style="float:left;margin:5px;display:none;" onclick="this.form.submit()" id="select{{$row->std_id}}">
+                </td>
+              </form>
               </tr>
+
               @endforeach
               @endif
             </table>
